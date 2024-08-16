5ac9eccdf028cee5893bdb74eeb47c74cc967b36
Suhail Shaji <suhailshaji@gmail.com>
2024-08-16 17:23:39 +0530

    feat: Add optional parameters and update version to 0.1.1
                - Added optional parameters to the phone number generator method in GenKit.
                - Improved functionality to save generated phone numbers as a list without errors.
                - Updated version to 0.1.1 to reflect minor improvements.
    
                Date: Aug 16, 2024

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genkit',
-    version='0.1.3',
+    version='0.1.4',
     description='A Python package for generating random data like phone numbers .',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
